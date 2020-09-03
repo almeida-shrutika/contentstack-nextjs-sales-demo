5c734c9287f6d60a61f3161259409ab6d386297e
Shrutika Almeida <almeidashrutika4497@gmail.com>
2020-09-03 06:53:13 +0000

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+console.log(process.env.test, process.env.test2, process.env.test3, 'env variables');
+module.exports = {
+  env: {
+    api_key: "bltbbf9dda285996efa",
+    delivery_token: "csdc08bd4d7296c30e74f83772",
+    environment: "production",
+  },
+};
